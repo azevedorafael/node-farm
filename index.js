7b70074a5fb4677067ca90f2bc2bbda9312c72ac
azevedorafael <you@example.com>
2020-03-22 18:58:44 -0300

    Building a (Very) Simple API

--- index.js
+++ index.js
@@ -33,6 +33,9 @@ const url = require('url');
 
 ///////////////////////////////////////
 //SERVER
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
@@ -41,6 +44,12 @@ const server = http.createServer((req, res) => {
   }
   else if (pathName === '/product') {
     res.end('This is the PRODUCT');
+  }
+  else if (pathName === '/api') {
+    res.writeHead(200, {
+      'Content-type': 'application/json',
+    })
+    res.end(data);
   }
   else {
     res.writeHead(404, {
